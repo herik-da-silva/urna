cabb9f982a1ecd91e69eed5f8413d3f6de55f517
Herik da Silva <heeikslv@gmail.com>
2021-01-13 17:35:57 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Bojack Horseman',
+                partido: 'ABC',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Princess Carolyn',
+                partido: 'CBA',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Todd Chavez',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Mr. Peanutbutter',
+                partido: 'QRS',
+                vice: 'Zul',
+                fotos: [
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+        ]
+    }
+
+];
